d8a8ce380a90f116d968b050e19e5373571221a3
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-01-27 03:01:07 +0000

    .

--- NUS Stuff/DSA1101/1819_Sem1/Tutorials/Tut01-02/Tut01.R
+++ NUS Stuff/DSA1101/1819_Sem1/Tutorials/Tut01-02/Tut01.R
@@ -1,4 +1,4 @@
-college_txt <- "/workspaces/devcontainers-rstudio/NUS Stuff/DSA1101/DSA1101 AY1819 Semester 1 (Dr. Sun Baoluo)/Tutorials/Tut01-02/Colleges.txt"
+college_txt <- "/workspaces/devcontainers-rstudio/NUS Stuff/DSA1101/1819_Sem1/Tutorials/Tut01-02/Colleges.txt"
 col <- read.table(college_txt, header = T, sep = "\t")
 View(col)
 
@@ -10,3 +10,7 @@ hist(col$DPerStudent[col$School_Type!="LibArts"])
 
 hist(col$GradPer[col$School_Type=="Lib Arts"])
 hist(col$GradPer[col$School_Type!="LibArts"])
+
+col$School[col$PerPhD>75] #institutions have more than 75% faculty members with PhD
+fit=lm(Acceptance~Top.10p+ PerPhD + GradPer ,data=col)
+summary(fit)#show result
--- NUS Stuff/DSA1101/Tutorial/Tutorial 1/Tut1.R
+++ NUS Stuff/DSA1101/Tutorial/Tutorial 1/Tut1.R
@@ -1,22 +1,20 @@
 # 1)
 numOfMonths <- function(salary, start, price, rate) {
-  # # 1i)
-  downPay <- 0.25 * price
-  setAside <- 0
-  withInterest <- start
-  totalSaved <- 0
-  numMonths <- 0
-  while (totalSaved < downPay) {
-    numMonths <- numMonths + 1
-    setAside <- setAside + 0.4 * salary
-    withInterest <- withInterest * 1.02
-    totalSaved <- setAside + withInterest
-    # #1ii)
-    if (numMonths %% 4 == 0) {
-      salary <- salary * (1 + rate)
+    # # 1i)
+    downPay <- 0.25 * price
+    portion_saved <- 0.4
+    with_interest <- 1.02
+    totalSaved <- start
+    numMonths <- 0
+    while (totalSaved < downPay) {
+        numMonths <- numMonths + 1
+        totalSaved <- totalSaved * with_interest + salary * portion_saved
+        # #1ii)
+        if (numMonths %% 4 == 0) {
+            salary <- salary * (1 + rate)
+        }
     }
-  }
-  return(numMonths)
+    return(numMonths)
 }
 
 price <- 1200000
