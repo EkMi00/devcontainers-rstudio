7de09f9294d3def2a818d6ba89a4e05aa5b271f3
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-02-02 12:37:16 +0000

    .

--- NUS Stuff/DSA1101/Tutorial/Tutorial 2/Tut2.R
+++ NUS Stuff/DSA1101/Tutorial/Tutorial 2/Tut2.R
@@ -6,16 +6,21 @@ fev_freq <- fev$FEV
 #      freq = TRUE, main = paste("Histogram of FEV"),
 #      xlab = "", ylab = "Frequency", axes = TRUE, col = "white"
 # )
+
+
 # boxplot(fev_freq, xlab = "FEV")
+
+
 out <- boxplot.stats(fev_freq)$out
 outliers <- which(fev_freq %in% c(out))
 # print(outliers) # Rows: 321 452 464 517 609 624 632 648 649
 # print(fev[outliers, ]) # Prints outlier rows
 
+
 # qqnorm(fev_freq, main = "FEV QQ Plot", pch = 20)
 # qqline(fev_freq, col = "red")
 
-# fev_male <- filter(fev, "FEV" >= "1",)
+
 fev_male <- fev[fev$Sex == 1, ]$FEV
 fev_female <- fev[fev$Sex == 0, ]$FEV
 # hist(fev_male,
@@ -27,30 +32,41 @@ fev_female <- fev[fev$Sex == 0, ]$FEV
 #     xlab = "", ylab = "Frequency", axes = TRUE, col = "pink"
 # ) # Left Skew
 
+
 # plot(fev$height, fev_freq, pch = 20, col = "darkblue")
 # print(cor(fev$height, fev_freq)) # r = 0.8675619
 
-# library("r2r")
-table <- hashmap()
+
 fib <- function(n) {
-    str_n <- toString(n)
-    helper <- function(n) {
-        if (n < 2) {
-            return(n)
-        } else if (n %in% names(table)) {
-            return(table[[str_n]])
-        } else {
-            table[[str_n]] <- (fib(n - 1) + fib(n - 2))
-            return(table[[str_n]])
-        }
+    if (n < 2) {
+        return(n)
     }
-    return(helper(n))
+    a <- 0
+    b <- 1
+    for (i in 2:n + 1) {
+        c <- a + b
+        a <- b
+        b <- c
+    }
+    return(b)
 }
+
+
 fibos <- c()
-for (i in 1:20) {
+for (i in 1:45) {
     fibos <- append(fibos, fib(i))
 }
 print(fibos)
 
-# 40th Fn = 63245986
+
+print(fib(40)) # 102334155
+
+
+i <- 1
+while (fib(i) < 5000000) {
+    i <- i + 1
+}
+print(i)
 # Smallest n such that Fn > 5000000: n = 34
+# print(fib(34))
+# print(fib(33))
