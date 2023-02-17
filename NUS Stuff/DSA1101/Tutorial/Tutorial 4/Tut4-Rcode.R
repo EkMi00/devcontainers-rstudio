5cf48c81e7ef8ad936b20afdfdb840cb7ff90d42
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-02-18 01:12:32 +0800

    .

--- NUS Stuff/DSA1101/Tutorial/Tutorial 4/Tut4-Rcode.R
+++ NUS Stuff/DSA1101/Tutorial/Tutorial 4/Tut4-Rcode.R
@@ -3,29 +3,31 @@
 
 
 ##########   Q1
-dat= read.table("C:/Data/Colleges.txt",header =TRUE,sep= "\t")
+dat= read.table("/mnt/c/Users/Keck/Documents/GitHub/Devcontainers-Rstudio/NUS Stuff/DSA1101/Data/Colleges.txt",header =TRUE,sep= "\t")
 names(dat)
 head(dat)
 
+# print(dat)
 matrix <- function(x, y) {
 beta <- solve(t(x )%*% x )%*% t(x )%*% y
 return( beta )
 }
 matrix( x = cbind (1,dat$SAT),y = dat$Acceptance )
 
-lm(Acceptance ~SAT , data =dat )
-
+m1 <- lm(dat$Acceptance ~dat$SAT , data =dat )
+# print(m1)
 
 matrix( cbind (1, dat$SAT ,dat$Top.10p), dat$Acceptance )
 # Compare outputs with lm()
-lm(Acceptance ~ SAT +Top.10p, data = dat )
+m2 <- lm(Acceptance ~ SAT +Top.10p, data = dat )
+# print(m2)
 
 
 ##########   Q2
 
-house = read.csv("C:/Data/house_selling_prices_FL.csv")
-names(house) # names of columns
-dim(house) # 100 observations and 9 columns
+house = read.csv("/mnt/c/Users/Keck/Documents/GitHub/Devcontainers-Rstudio/NUS Stuff/DSA1101/Data/house_selling_prices_FL.csv")
+# names(house) # names of columns
+# dim(house) # 100 observations and 9 columns
 house$NW = as.factor(house$NW) # to declare that NW is categorical
 attach(house)
 
@@ -34,21 +36,21 @@ attach(house)
 cor(price, size)
 
 #(b)
-plot(size, price, pch = 20)
+# plot(size, price, pch = 20)
 
 #(c)
 M1 = lm(price ~ size, data = house)
-
 summary(M1)
 
 
 #(d)
 M2 = lm(price ~ size + NW, data = house)
-summary(M2)
+print(summary(M2))
+print(M2)
 
 
 #(f)
-predict(M2, newdata=data.frame(size=4000, NW = "1"))
+print(predict(M2, newdata=data.frame(size=4000, NW = "1")))
 
 
 
--- NUS Stuff/DSA1101/Tutorial/Tutorial 4/Tut4.R
+++ NUS Stuff/DSA1101/Tutorial/Tutorial 4/Tut4.R
@@ -1,65 +1,51 @@
 library("ggplot2")
-setwd("/mnt/c/Users/Keck/Documents/GitHub/myrepo/Rstudio Projects/NUS Stuff/DSA1101/Data");
+setwd("/mnt/c/Users/Keck/Documents/GitHub/Devcontainers-Rstudio/NUS Stuff/DSA1101/Data");
 
-colleges <- read.csv("Colleges.txt", sep = "\t" , header = T ,
-                     na.strings ="", 
-                     stringsAsFactors= F)
+##### 1 ######
+colleges <- read.table("Colleges.txt", sep = "\t" , header = T)
 
 # print(head(colleges))
+matrix <- function(x, y) {
+beta <- solve(t(x )%*% x )%*% t(x )%*% y
+return( beta )
+}
+m1 <- lm(Acceptance ~ SAT, data = colleges)
+print(m1)
 
-y <- (colleges$Acceptance)
-x0 <- seq(1, 1, length.out=nrow(colleges))
-x1 <- colleges$SAT
-
-X_1 <- cbind(x0, x1)
-
-
-X_1T <- t(X_1)
-LRSS <- solve(X_1T%*%X_1, X_1T%*%y)
+LRSS <- matrix(cbind(1,dat$SAT), dat$Acceptance)
 # print(LRSS) 
-# b0 = 42732.0424
-# b1 = -329.9377
-
-x2 <- (colleges$Top.10p)  
-
-X_2 <- cbind(x0, x1, x2)
-X_2T <- t(X_2)
-LRSS2 <- solve(X_2T%*%X_2, X_2T%*%y)
-
-# print(LRSS2)
+# b0 = 202.2677440
+# b1 = -0.1300894
 
 m2 <- lm(colleges$Acceptance ~ colleges$SAT + colleges$Top.10p, data = colleges)
 # print(m2)
 
-resale <- read.csv("house_selling_prices_FL.csv", sep = "," , header = T ,
-                     na.strings ="", 
-                     stringsAsFactors= F)
 
+
+##### 2 #####
+
+resale <- read.csv("house_selling_prices_FL.csv", sep = "," , header = T)
+
+resale$NW = as.factor(resale$NW) # to declare that NW is categorical
+attach(resale)
 # print(head(resale))
 
 price_size <- lm(resale$price ~ resale$size, data = resale)
-
-# print(price_size)
 
 my_graph <- ggplot(resale, aes(x = resale$size, y = resale$price)) + 
     geom_point() + 
     geom_smooth(method=lm , color="red", se=FALSE)
     # + stat_smooth()
-
 # print(my_graph) # Postive assoication
 
 rsq <- function(x, y) cor(x, y) ^ 2
 # print(rsq(resale$size, resale$price))
 
-model2 <- lm(resale$price ~ resale$size + resale$NW, data=resale)
-# print(model2) #NW = 30569.08729 
-b0 = model2$coefficients["(Intercept)"]
-b1 = model2$coefficients["resale$size"]
-b2 = model2$coefficients["resale$NW"]
+model2 <- lm(price ~ size + NW, data=resale)
+print(model2) #NW = 30569.08729 
 
-projected_y <- b0 + b1 * (4000) + b2 * (1)
+print(predict(model2, newdata=data.frame(size=4000, NW = "1")))
 
-# print(projected_y)
 
 print(summary(model2)) # 0.6276
 
