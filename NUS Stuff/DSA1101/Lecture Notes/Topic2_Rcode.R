69d7ffe634f0a8c3041aa6f86cb4c8bee01325d2
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-02-22 13:55:14 +0800

    .

--- NUS Stuff/DSA1101/Lecture Notes/Topic2_Rcode.R
+++ NUS Stuff/DSA1101/Lecture Notes/Topic2_Rcode.R
@@ -1,7 +1,7 @@
 ####  TOPIC 2: BASIC PROBABILITY & STATISTICS
 
 # setwd("C:/Data")
-setwd("/workspaces/devcontainers-rstudio/NUS Stuff/DSA1101/Data")
+setwd("/mnt/c/Users/Keck/Documents/GitHub/Devcontainers-Rstudio/NUS Stuff/DSA1101/Data")
 sales <- read.csv("yearly_sales.csv")
 
 head(sales)
@@ -21,37 +21,37 @@ total[order(total)[1:5]] # The 5 smallest observations
 total[order(total)[(n - 4):n]] # The 5 largest observations
 
 # HISTOGRAM
-hist(total,
-     freq = TRUE, main = paste("Histogram of Total Sales"),
-     xlab = "total", ylab = "Frequency", axes = TRUE, col = "blue"
-)
+# hist(total,
+#      freq = TRUE, main = paste("Histogram of Total Sales"),
+#      xlab = "total", ylab = "Frequency", axes = TRUE, col = "blue"
+# )
 
 # HISTOGRAM WITH DENSITY LINE
-hist(total,
-     freq = FALSE, main = paste("Histogram of total sales"),
-     xlab = "total", ylab = "Probability", axes = TRUE,
-     col = "blue", ylim = c(0, 0.0045)
-)
-lines(density(total), col = "red") # this is the density curve of "total"
+# hist(total,
+#      freq = FALSE, main = paste("Histogram of total sales"),
+#      xlab = "total", ylab = "Probability", axes = TRUE,
+#      col = "blue", ylim = c(0, 0.0045)
+# )
+# lines(density(total), col = "red") # this is the density curve of "total"
 
 # HISTOGRAM WITH NORMAL DENSITY
-hist(total,
-     freq = FALSE, main = paste("Histogram of Total Sales"),
-     xlab = "total sales", ylab = "Probability", axes = TRUE,
-     col = "grey", ylim = c(0, 0.002)
-)
-x <- seq(0, max(total), length.out = n)
-y <- dnorm(x, mean(total), sd(total))
-lines(x, y, col = "red") # this is the normal density curve
+# hist(total,
+#      freq = FALSE, main = paste("Histogram of Total Sales"),
+#      xlab = "total sales", ylab = "Probability", axes = TRUE,
+#      col = "grey", ylim = c(0, 0.002)
+# )
+# x <- seq(0, max(total), length.out = n)
+# y <- dnorm(x, mean(total), sd(total))
+# lines(x, y, col = "red") # this is the normal density curve
 
 
 # BOX PLOTS
-boxplot(total, xlab = "Total Sales")
+# boxplot(total, xlab = "Total Sales")
 
 
 # QQ plot
-qqnorm(total, main = "QQ Plot", pch = 20)
-qqline(total, col = "red")
+# qqnorm(total, main = "QQ Plot", pch = 20)
+# qqline(total, col = "red")
 
 
 
@@ -61,7 +61,7 @@ cor(total, order) # 0.75
 
 
 # SCATTER PLOT
-plot(order, total, pch = 20, col = "darkblue")
+# plot(order, total, pch = 20, col = "darkblue")
 
 
 # BOX PLOTS OF MULTIPLE GROUP
@@ -72,7 +72,7 @@ boxplot(total ~ sales$gender)
 # 3 VARIABLES = SCATTER PLOT ADDING LEGEND
 order <- sales$num_of_orders
 attach(sales)
-plot(order, total, type = "n") # a scatter plot with no point added
+# plot(order, total, type = "n") # a scatter plot with no point added
 points(order[gender == "M"], total[gender == "M"], pch = 2, col = "blue") # MALE
 points(order[gender == "F"], total[gender == "F"], pch = 20, col = "red") # FEMALE
 legend(1, 7500, legend = c("Female", "Male"), col = c("red", "blue"), pch = c(20, 2))
@@ -87,7 +87,7 @@ count # frequency table
 barplot(count)
 
 # PIE CHART
-pie(count)
+# pie(count)
 
 
 # CATEGORIZING "ORDER"
