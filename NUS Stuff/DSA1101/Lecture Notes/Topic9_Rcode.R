50975a4dbcf797be1c79d04fbda9e16168cde426
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-03-29 13:56:17 +0800

    .

---
+++ NUS Stuff/DSA1101/Lecture Notes/Topic9_Rcode.R
@@ -0,0 +1,170 @@
+
+######  ASSOCIATION RULES 
+
+
+#install.packages('arules')
+#install.packages('arulesViz')
+
+# documentation of package 'arules'
+# https://cran.r-project.org/web/packages/arules/arules.pdf
+
+library('arules')
+library('arulesViz')
+
+data(Groceries)
+
+?Groceries
+
+summary(Groceries)
+
+# this link below is helpful to understand this special data
+# https://www.jdatalab.com/data_science_and_data_mining/2018/10/10/association-rule-transactions-class.html#:~:text=The%20Transactions%20Class,-The%20arules%20package&text=The%20Groceries%20data%20set%20contains,to%20read%20the%20Groceries%20data.
+
+inspect(head(Groceries)) # the first 6 transactions
+
+
+
+Groceries@itemInfo[1:10,]
+Groceries@data[,100:110]
+
+# the items for first 5 transactions:
+apply(Groceries@data[,1:5], 2,
+      function(r) paste(Groceries@itemInfo[r,"labels"], collapse=", "))
+
+# the items for 100th to 105-th transactions:
+apply(Groceries@data[,100:105], 2,
+      function(r) paste(Groceries@itemInfo[r,"labels"], collapse=", "))
+
+
+
+###############  GETTING THE FREQUENT 1-ITEMSETS:
+
+itemsets.1 <- apriori(Groceries, parameter=list(minlen=1, maxlen=1,
+           support=0.02, target="frequent itemsets"))
+
+summary(itemsets.1)
+
+# minlen = 1: frequent itemset has at least 1 item
+# maxlen = 1: frequent itemset has max = 1 item
+# set both 'minlen = 1' and 'maxlen = 1' means we want frequent itemset that has only 1 item.
+
+
+# list the most 10 frequent 1-itemsets:
+inspect(head(sort(itemsets.1, by = "support"), 10))
+
+# list all the 59 frequent 1-itemsets:
+inspect(sort(itemsets.1, by ="support"))
+
+
+
+###############  GETTING THE FREQUENT 2-ITEMSETS:
+
+itemsets.2 <- apriori(Groceries, parameter=list(minlen=2, maxlen=2,
+          support=0.02, target="frequent itemsets"))
+
+summary(itemsets.2)
+
+# list all the frequent 2-itemsets:
+inspect(sort(itemsets.2, by ="support"))
+
+# list of most 10 frequent 2-itemsets:
+inspect(head(sort(itemsets.1, by = "support"), 10))
+
+
+
+###############  GETTING THE FREQUENT 3-ITEMSETS:
+
+
+itemsets.3 <- apriori(Groceries, parameter=list(minlen=3, maxlen=3,
+          support=0.02, target="frequent itemsets"))
+
+summary(itemsets.3)
+inspect(sort(itemsets.3, by ="support"))
+
+# only TWO frequent itemsets that meets the minimum support of 0.02.
+
+
+###############  GETTING THE FREQUENT 3-ITEMSETS:
+
+itemsets.4 <- apriori(Groceries, parameter=list(minlen=4, maxlen=4,
+    support=0.02, target="frequent itemsets"))
+
+summary(itemsets.4)
+
+inspect(sort(itemsets.4, by ="support")) # nothing
+
+# no 4-itemset satisfies the minimum support of 0.02. 
+# If we lower down the minimum support to 0.007 then....
+
+itemsets.4 <- apriori(Groceries, parameter=list(minlen=4, maxlen=4,
+    support=0.007, target="frequent itemsets"))
+
+summary(itemsets.4)
+inspect(sort(itemsets.4, by ="support"))
+
+# there are three frequent 4-itemsets if the minimum support is 0.007.
+
+
+## # if the parameter maxlen is not specified, then....
+
+itemsets<- apriori( Groceries , parameter = list( minlen=1,
+            support =0.02 , target ="frequent itemsets"))
+
+summary( itemsets )
+# this summarizes that: there are 59 frequent 1-itemsets; 
+# 61 frequent 2-itemsets; and 2 frequent 3-itemsets
+
+inspect(sort( itemsets , by ="support")) 
+# this will rank the itemsets by their support, regardless of itemsets with 1 item or 2 items.
+# row 17: {other vegetables, whole milk}  with support = 0.07483477
+
+
+
+
+###############  GETTING THE RULES instead of  FREQUENT ITEMSETS
+
+rules <- apriori(Groceries, parameter=list(support=0.001,
+         confidence=0.6, target = "rules"))
+
+plot(rules) $ scatter plot of all 2918 rules
+
+# Scatter plot with custom measures and can add limiting the plot to the 100 with the 
+# largest value for for the shading measure. 
+plot(rules, measure = c("support", "confidence"), shading = "lift", col = "black")#, limit = 100)
+
+# more information about plot() under 'arules':
+# http://127.0.0.1:23659/library/arulesViz/html/plot.html
+
+
+
+
+# PLOT SOME TOP RULES FOR VISUALZATION:
+
+# the top 3 rules sorted by LIFT:
+inspect(head(sort(rules, by="lift"), 3))
+
+# the top 5 rules sorted by LIFT
+inspect(head(sort(rules, by="lift"), 5))
+highLiftRules <- head(sort(rules, by="lift"), 5)
+
+# plot the top 5 rules above for visualzation:
+plot(highLiftRules, method="graph") # this is simple and a bit difficult to see
+
+# more parameters added, plot looks better:
+plot(highLiftRules, method = "graph", engine = "igraph",
+     edgeCol = "blue", alpha = 1)
+# alpha = c(0,1)
+# the size of the node is sorted by the support.
+# the darkness of the color represents the change in lift
+
+
+plot(highLiftRules, method = "graph", engine = "igraph",
+  nodeCol = "red", edgeCol = "blue", alpha = 1)
+# this will fix the color be "red" for all lift values, 
+# only the size of the node is sorted by the support.
+
+
+
+#some common choices for 'method':
+# matrix, mosaic, doubledecker, graph, paracoord, scatterplot, grouped matrix, two-key plot, matrix3D
+
---
+++ NUS Stuff/DSA1101/Tutorial/Tutorial 8/Tut8-Rcode.R
@@ -0,0 +1,163 @@
+
+###### Q1:  TITANIC DATA SET & NAIVE BAYES
+
+
+titanic= read.csv("C:/Data/Titanic.csv")
+dim(titanic)
+head(titanic)
+
+attach(titanic)
+
+table(Survived)
+
+# (a) Compute the probabilities P(Y = 1) (survived) and P(Y = 0) (did not survive).
+
+tprior <- table(Survived) # Number of ppl survived & not survived
+tprior
+tprior <- tprior/sum(tprior) # the probability scores
+tprior
+
+
+# (b) Compute the conditional probabilities P(Xi = xi|Y = 1) and P(Xi = xi|Y = 0):
+
+classCounts <- table(titanic[,c("Survived", "Class")]); classCounts
+classCounts <- classCounts/rowSums(classCounts); classCounts
+
+genderCounts <- table(titanic[,c("Survived", "Sex")]); genderCounts
+genderCounts <- genderCounts/rowSums(genderCounts) ; genderCounts
+
+ageCounts <- table(titanic[,c("Survived", "Age")]); ageCounts
+ageCounts <- ageCounts/rowSums(ageCounts); ageCounts
+
+
+# (c) Predict survival for an adult female passenger in 2nd class cabin.
+
+prob_survived <-
+classCounts["Yes","2nd"]*
+genderCounts["Yes","Female"]*
+ageCounts["Yes","Adult"]*
+tprior["Yes"]
+
+prob_survived
+
+
+prob_not_survived <-
+classCounts["No","2nd"]*
+genderCounts["No","Female"]*
+ageCounts["No","Adult"]*
+tprior["No"]
+
+
+prob_survived
+prob_not_survived
+
+prob_survived/prob_not_survived
+
+
+# (d) Compare your prediction above with the one performed by the naiveBayes()
+#function in package `e1071'
+library(e1071)
+
+M1 <- naiveBayes(Survived ~., titanic)
+
+test <- data.frame(Class="2nd", Sex="Female", Age="Adult")
+
+results <- predict(M1,test)
+results
+results <- predict(M1,test, "raw")
+results
+
+#ratio of probability score
+prob_survived/prob_not_survived
+
+#ratio of actual probabilities
+results[1,"Yes"]/results[1,"No"]
+
+# conclude: same answer
+
+
+##################### Q2: TITANIC DATA SET & NAIVE BAYES & LOGISTIC REGRESSION
+
+# Naive Bayes classification
+M1 <- naiveBayes(Survived ~., titanic)
+
+
+# (a) Logistic regression
+# response must be of 0 and 1 to fit the model
+
+sur = (Survived == 'Yes')
+titanic$sur = sur ; head(titanic)
+M2 <- glm(sur~ Class + Sex + Age, data=titanic, family=binomial(link= "logit"))
+
+summary(M2)
+
+# (b) write down the fitted model, phat = predicted probability of survival:
+
+# log[phat/(1-phat)] = 2.0438 -1.0181*I(Class = 2nd) -1.7778*I(Class = 3rd) 
+#                   -0.8577* I(Class = Crew) -2.4201*I(Sex = Male) + 1.0615*I(Age = Child)
+
+# (c) Interpret the coefficient of variable SEX:
+# coefficient is estimated = -2.4201. It means, given the same condition on the class and age,
+# when comparing to a female, the LOG-ODDS of survivalfor a male is less than by 2.42.
+# It means, the ODDS of survival of a male passenger will be less than that of a female by
+# e^2.42 = 11.25 TIMES.
+
+
+# (d) Interpret the coefficient of variable AGE:
+# coefficient is estimated = 1.0615. It means, given the same condition on the class and gender,
+# when comparing to an adult, the LOG-ODDS of survival of a child is larger by 1.0615.
+# That means, the ODDS of survival of a child passenger is larger than that of an adult passenger by 
+# e^1.0615 = 2.89 TIMES.
+
+
+# (e) ROC & AUC
+#different classfiers produce different performances
+#plot 2 ROC curves in the same plane
+
+library(ROCR)
+
+# ROC for Logistic Regresison:
+pred = predict(M2, type="response")
+predObj = prediction(pred, titanic$Survived)
+rocObj = performance(predObj, measure="tpr", x.measure="fpr")
+plot(rocObj)
+
+auc1 = performance(predObj , measure ="auc")
+auc1@y.values[[1]] # 0.7597259
+
+
+
+
+# ROC for Naive Bayes classifier
+naiveB <- predict(M1, titanic[,1:3],type='raw')
+score <- naiveB[, 2]
+pred_nb <- prediction(score, titanic$sur)
+roc_nb = performance(pred_nb, measure="tpr", x.measure="fpr")
+plot(roc_nb, add = TRUE, col = "red") #add = TRUE means plot the curve in the existing plot
+
+auc2 <- performance(pred_nb , "auc")@y.values[[1]]
+auc2 #0.7164944
+
+
+legend("bottomright", c("logistic regression","naive Bayes"),col=c("black","red"), lty=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
