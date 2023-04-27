440f70adca4710d1a1acc0020dc63023c7bbff6b
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-04-27 10:01:45 +0800

    Update Tut11Practise.R

--- NUS Stuff/DSA1101/Tutorial/Tutorial 11/Tut11Practise.R
+++ NUS Stuff/DSA1101/Tutorial/Tutorial 11/Tut11Practise.R
@@ -75,10 +75,16 @@ apply(Groceries@data[,1:5], 2,
 apply(Groceries@data[,100:105], 2,
       function(r) paste(Groceries@itemInfo[r,"labels"], collapse=", "))
 
-suppressWarnings(   
-itemset.1 <- apriori(Groceries,
-    parameter=list(minlen=1, maxlen=1, support=0.02, target="frequent itemsets")) 
-)
+# print(size(Groceries)[1:100]) # size of each transactions
+
+
+
+# suppressWarnings(   
+# itemset.1 <- apriori(Groceries,
+#     parameter=list(minlen=1, maxlen=1, support=0.02, target="frequent itemsets")) 
+# )
+# inspect(Groceries[which(size(Groceries) == 1),]) # get all 1-itemsets using index
+
 
 # print(summary(itemset.1))
 
@@ -91,8 +97,8 @@ itemset.1 <- apriori(Groceries,
 
 
 
-itemsets.2 <- apriori(Groceries, parameter=list(minlen=2, maxlen=2,
-          support=0.02, target="frequent itemsets"))
+# itemsets.2 <- apriori(Groceries, parameter=list(minlen=2, maxlen=2,
+#           support=0.02, target="frequent itemsets"))
 
 # print(summary(itemsets.2))
 
@@ -102,27 +108,27 @@ itemsets.2 <- apriori(Groceries, parameter=list(minlen=2, maxlen=2,
 # list of most 10 frequent 2-itemsets:
 # inspect(head(sort(itemsets.2, by = "support"), 10))
 
-itemsets.3 <- apriori(Groceries, parameter=list(minlen=3, maxlen=3,
-          support=0.02, target="frequent itemsets"))
+# itemsets.3 <- apriori(Groceries, parameter=list(minlen=3, maxlen=3,
+#           support=0.02, target="frequent itemsets"))
 
-summary(itemsets.3)
+# summary(itemsets.3)
 # inspect(sort(itemsets.3, by ="support"))
 
 
 
-itemsets.4 <- apriori(Groceries, parameter=list(minlen=4, maxlen=4,
-    support=0.02, target="frequent itemsets"))
+# itemsets.4 <- apriori(Groceries, parameter=list(minlen=4, maxlen=4,
+#     support=0.02, target="frequent itemsets"))
 
-summary(itemsets.4)
+# summary(itemsets.4)
 
 # inspect(sort(itemsets.4, by ="support")) # nothing
 
 
 
-itemsets.4 <- apriori(Groceries, parameter=list(minlen=4, maxlen=4,
-    support=0.007, target="frequent itemsets"))
+# itemsets.4 <- apriori(Groceries, parameter=list(minlen=4, maxlen=4,
+#     support=0.007, target="frequent itemsets"))
 
-summary(itemsets.4)
+# summary(itemsets.4)
 # inspect(sort(itemsets.4, by ="support"))
 
 # there are three frequent 4-itemsets if the minimum support is 0.007.
@@ -143,38 +149,51 @@ summary( itemsets )
 rules <- apriori(Groceries, parameter=list(support=0.001,
          confidence=0.6, target = "rules"))
 
-#     lhs                              rhs                support     confidence
-# [1] {honey}                       => {whole milk}       0.001118454 0.7333333
-# [2] {cereals}                     => {whole milk}       0.003660397 0.6428571
-# [3] {rice}                        => {whole milk}       0.004677173 0.6133333
-# [4] {liver loaf, yogurt}          => {whole milk}       0.001016777 0.6666667
-# [5] {tropical fruit, curd cheese} => {other vegetables} 0.001016777 0.6666667
-# [6] {curd cheese, rolls/buns}     => {whole milk}       0.001016777 0.6250000
-#     coverage    lift     count
-# [1] 0.001525165 2.870009 11
-# [2] 0.005693950 2.515917 36
-# [3] 0.007625826 2.400371 46
-# [4] 0.001525165 2.609099 10
-# [5] 0.001525165 3.445437 10
-# [6] 0.001626843 2.446031 10
-plot(rules)
-plot(rules, measure= c("support", "confidence"),
-     shading="lift", col="red") # scatter plot of all 2918 rules
+rules_df <- DATAFRAME(rules, setStart='', setEnd='', separate = TRUE) # DATAFRAME must be captial as it is a separate function
+
+# plot(rules)
+# plot(rules, measure= c("support", "confidence"),
+#      shading="lift", col="red") # scatter plot of all 2918 rules
+
+# # Check if a string of charaters is in another string
+# string1 <- "Programiz"
+
+# value1 <- "miz"
+
+# # check if "miz" is present in "Programiz"
+# grepl(value1, string1)  # TRUE
+
+# value2 <- "grm"
+
+# # check if "grm" is present in "Programiz"
+# grepl(value2, string1)  # FALSE
+
+# inspect(rules[which(size(rules@lhs) < 3),]) # which items in lhs have less than 3 items in transactions
+# inspect(subset(rules, subset=lhs %in% "liver loaf")) # %pin% finds all sets with {"liver loaf", ...}
+
+# inspect(subset(rules, subset=lhs %ain% c("liver loaf", "yogurt"))) # %pin is partial matching, must have all in 
+# inspect(subset(rules, subset=lhs %ain% c("honey"))) # %ain% is partial matching, must have all in lhs/rhs
+# inspect(subset(rules, subset=lhs %pin% "honey")) # %pin% is partial matching, any with 1 specified object in lhs/rhs
+
+# inspect(subset(rules, subset=lhs %oin% "citrus fruit"))  # %oin% finds items only with item specified 
+# inspect(subset(rules, subset=lhs %pin% "citrus fruit")) # %pin% finds all sets with {citrus fruit, ...}, similar to %in%
+# inspect(subset(rules, subset=lhs %pin% "citrus")) # %pin% finds all sets with string "citrus" in {citrus ... , ...}, different from %in% 
+# inspect(subset(rules, subset=lhs %oin% "citrus"))  # no item called "citrus" different from %pin%
 
 
 # the top 3 rules sorted by LIFT:
-inspect(head(sort(rules, by="lift"), 3))
+# inspect(head(sort(rules, by="lift"), 3))
 
 # the top 5 rules sorted by LIFT
-inspect(head(sort(rules, by="lift"), 5))
+# inspect(head(sort(rules, by="lift"), 5))
 highLiftRules <- head(sort(rules, by="lift"), 5)
 
 # plot the top 5 rules above for visualzation:
 # plot(highLiftRules, method="matrix") # this is simple and a bit difficult to see
 
 # more parameters added, plot looks better:
-plot(highLiftRules, method = "graph", engine = "igraph",
-     edgeCol = "blue", alpha = c(1))
+# plot(highLiftRules, method = "graph", engine = "igraph",
+#      edgeCol = "blue", alpha = c(1))
 # alpha = c(0,1)
 # the size of the node is sorted (determined?) by the support.
 # the darkness of the color represents the change in lift
