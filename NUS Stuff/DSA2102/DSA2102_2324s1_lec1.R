d16f51a378f09231d232226ef7fd369a27c2d692
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-08-11 21:37:21 +0800

    .

---
+++ NUS Stuff/DSA2102/DSA2102_2324s1_lec1.R
@@ -0,0 +1,41 @@
+# Write a function that takes a number as an input and returns its square
+
+sq <- function(x) {x^2}
+# print(sq(3))
+# print(sq(-pi))
+
+# Write a for loop to compute the sum of the first n positive integers
+
+n <- 10
+s <- 0
+for (i in 1:n){
+  s <- s + i
+}
+# print(s)
+
+# Write a function that takes in two vectors and computes their dot product
+
+dot <- function(x,y) {sum(x*y)}
+
+x <- c(1,-2,3)
+y <- c(1,1,1)
+
+# print(dot(x,y))
+print(dot(pi,x))
+# print(dot("a",x))
+
+dot <- function(x,y){
+  if (is.numeric(x) & is.numeric(y) & length(x) == length(y)){
+    n = length(x)
+    result <- 0
+    for (i in 1:n){
+      result <- result + x[i]*y[i]
+    }
+    return(result)
+  }
+  else  { return("error") }
+}
+
+# print(dot(x,y))
+print(dot(pi,x))
+# print(dot("a",x))
---
+++ NUS Stuff/DSA2102/DSA2102_2324s1_lec2.R
@@ -0,0 +1,10 @@
+library(float)
+
+0.23
+s1 = sprintf("%.23f",as.float(0.23))
+print(s1)
+0.25
+s2 = sprintf("%.23f",as.float(0.25))
+print(s2)
+print(as.float(1.728^as.float(1/3))*as.float(1.728^as.float(1/3))*as.float(1.728^as.float(1/3))-1.728)
+print(as.float(3.375^as.float(1/3))*as.float(3.375^as.float(1/3))*as.float(3.375^as.float(1/3))-3.375)
