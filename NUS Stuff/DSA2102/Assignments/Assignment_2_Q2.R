78f796bd8b6a30bbfe0217e54df9cc62a11c8de9
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-09-12 16:15:06 +0800

    Update Assignment_2_Q2.R

--- NUS Stuff/DSA2102/Assignments/Assignment_2_Q2.R
+++ NUS Stuff/DSA2102/Assignments/Assignment_2_Q2.R
@@ -1,72 +1,46 @@
 set.seed(123)
 
 getBanded <- function(n, w) {
-    M <- matrix(sample(-99:99, size = n*n), nrow=n)
+    M <- matrix(sample(-9:9, size = n*n, replace=TRUE), nrow=n)
     M[abs(row(M) - col(M)) > w] = 0
     return(M)
 } 
-w = 1
-rows <- 10
-M <- getBanded(rows, w)
-b <- 1:rows
+w = 2
+n <- 7
+M <- getBanded(n, w)
+b <- 1:n
 print(M)
+#      [,1] [,2] [,3] [,4] [,5]
+# [1,]    5    8    9    0    0
+# [2,]    9    1   -1   -1    0
+# [3,]    4   -5   -7    9   -3
+# [4,]    0    4   -2   -6    2
+# [5,]    0    0   -3    4    5
 
+LU <- M
+for (i in (1:(1+w))) {
+    for (j in (i+1):(i+w+1)) {
+        # m <- M[j,i]/M[i,i]
+        print(c(j,i))
+    }
+}
+
+# print(LU)
 my.elimination <- function(A,b,w) {
     A <- cbind(A,b)
     n <- nrow(A)
-    A[1,2] = A[1,2]/A[1,1]
-
-    # Diagonal 
-    for (i in 1:n) { 
-        for (j in 1:n) {
-            for (k in 1:(j-1)) {
-                A[i,i] - A[i,k]*A[k,i]
-            }
-        }
-    }
-    # Lower Sub-diagonal
-    for (i in 1+w:n) { 
-        for (j in 1:n) {
-            for (k in 1:(j-1)) {
-            }
-        }
-    }
-    for (i in 1:w) {
-        for (j in 1:(i+w)) {
-            # print(c(i,j))
-            for (k in i:i+w) {
-                # print(c(i,k, k, j))
-            }
-        }
-    }
-    for (i in (n-w+1):n) {
-        for (j in (i-w):n) {
-            # print(c(i,j))
-            
-        }
-    }
-    for (i in (1+w):(n-w)) {
-        for (j in (i-w):(i+w)) {
-            # print(c(i,j))
+    for (i in 1:(n-1)) {
+        for (j in (i+1):n) {
             m <- A[j,i]/A[i,i]
-            # A[j,i] <- m   
             for (k in (i+1):(n+1)) {
-                # A[j,k] <- A[j,k] - (m * A[i,k])
+                # print(c(j,k))
+                A[j,k] <- A[j,k] - (m*A[i,k])
             }
         }
     }
-  return(A)
+    # print(A[,-(n+1)])
+    return(A)
 }
-
-# my.elimination <- function(A, w) {
-#     A <- cbind(A)
-#     n <- nrow(A)
-#     L <- replicate(n, numeric(n))
-#     U <- replicate(n, numeric(n))
-
-    
-#   return(A)
-# }
 
 my.backsub <- function(U,b) {
     n <- nrow(U)
@@ -83,18 +57,18 @@ my.backsub <- function(U,b) {
     return(b)
 }
 
-LU <- (my.elimination(M,b,w))[,1:rows] 
+LU <- (my.elimination(M,b,w))[,1:n] 
 # print(LU)
 
 my.solve <- function(A,b){
   n <- nrow(A)
-  LU <- (my.elimination(A,b)) 
+  LU <- (my.elimination(A,b,w)) 
   U <- LU[,1:n]
   u <- LU[,n+1]
   my.backsub(U,u)
 }
 
-# print(solveLU(M, b))
+# print(my.solve(M, b))
 # print(solve(M, b))
 
 # print(getLU(M,b))
