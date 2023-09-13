3b56b7738863bafd28119099a300ec795c65a41a
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-09-14 02:21:44 +0800

    .

---
+++ NUS Stuff/DSA2102/Banded.R
@@ -0,0 +1,105 @@
+set.seed(1234)
+
+getBanded <- function(n, w) {
+    M <- matrix(sample((-9:9)[-10], size = n*n, replace=TRUE), nrow=n)
+    M[abs(row(M) - col(M)) > w] = 0
+    return(M)
+} 
+
+minor_reduction <- 
+
+my.elimination <- function(A,b,w) {
+	A <- cbind(A,b)
+    n <- nrow(A)
+    for (i in 1:(n-w)) {
+        for (j in (i+1):(i+w)) {
+            A[j,i] <- A[j,i]/A[i,i]
+            for (k in (i+1):(i+w)) {
+                A[j,k] <- A[j,k] - (A[j,i] * A[i,k])
+            }
+            A[j,n+1] <- A[j,n+1] - (A[j,i] * A[i,n+1]) # For the b vector
+        }
+    }
+    if (w != 1) {
+        for (i in (n-w+1):(n-1)) {
+            for (j in (i+1):n) {  
+                m <- A[j,i]/A[i,i]
+                for (k in (i+1):n) {
+                    A[j,k] <- A[j,k] - (m * A[i,k])
+                }
+                A[j,n+1] <- A[j,n+1] - (m * A[i,n+1]) # For the b vector
+            }
+        }
+    }
+    return(A)
+}
+
+my.backsub <- function(U, b, w) {
+    n <- nrow(U)    
+    # print(U)
+    for (j in n:(w+1)) {
+        if (U[j,j] == 0 | is.na(U[j,j]) | is.nan(U[j,j])) {
+            return("Error: singular or need permuting/pivoting")
+        }
+		else {
+            b[j] <- b[j]/U[j,j]
+            for (i in (j-w):(j-1)) {
+                b[i] <- b[i] - U[i,j] * b[j]
+            }
+		}
+	}
+    if (w != 1) {
+        for (j in w:2) {
+            if (U[j,j] == 0 | is.na(U[j,j]) | is.nan(U[j,j])) {
+                return("Error: singular or need permuting/pivoting")
+            }
+            else {
+                b[j] <- b[j]/U[j,j]
+                for (i in 1:(j-1)) {
+                    b[i] <- b[i] - U[i,j] * b[j]
+                }
+            }
+        }
+    }
+	b[1] <- b[1]/U[1,1]
+	return(b)
+}
+
+n <- 10
+w <- 1
+M <- getBanded(n,w)
+# print(M)
+b <- 1:n
+LU <- my.elimination(M, b, w)
+# print(LU)
+
+x <- my.backsub(LU[,1:n], LU[,n+1], w)
+print(x)
+print(solve(M,b))
+
+my.solve <- function(A,b,w){
+    n <- nrow(A)
+    LU <- (my.elimination(A,b,w)) 
+    U <- LU[,1:n]
+    u <- LU[,n+1]
+    my.backsub(U,u,w)
+}
+
+testCases <- function(n) {
+    w <- 1:(n-1)
+    for (i in w) {
+        M <- getBanded(n,i)
+		b <- 1:n
+        myans <- my.solve(M, b, i)
+        correct <- solve(M, b)
+        case <- sprintf("Case: %.0f x %.0f matrix, Bandwidth w = %.0f",n,n,i)
+        print(case)
+        # print("My Answer:" )
+        print(myans)
+        # print("Correct Answer:" )
+        print(correct)
+    }  
+}
+
+n <- 6
+print(testCases(n))
