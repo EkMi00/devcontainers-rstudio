cf643cb1e7aa0f7f97aa8c30665039b0d0c80d27
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2024-01-26 09:39:02 +0800

    .

---
+++ Hello World/Hello.R
@@ -0,0 +1,22 @@
+# setwd("C:\\Data")
+setwd("C:\\Users\\Keck\\Documents\\GitHub\\devcontainers-rstudio\\NUS Stuff\\DSA1101\\Data")
+# setwd("C:\\Users\\five8\\Documents\\GitHub\\devcontainers-rstudio\\NUS Stuff\\DSA1101\\Data")
+"Hello"
+8 + 3
+
+foo <- function() {
+    print("fuck me")
+}
+foo()
+
+v1 <- c(1, 2, 3)
+x <- cbind(1, v1)
+x
+
+x = c( -1, 0, 1, 2)
+y = c( -10, -4 , 0, 14) 
+lm(y~x)
+
+x_2 = x^2
+x = cbind(x, x_2)
+lm(y~x)
